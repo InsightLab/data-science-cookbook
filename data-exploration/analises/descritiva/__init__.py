ad0235551815170bcd1cb4b257006a7686883c0a
araujodavid <david.brasillo@gmail.com>
2016-10-03 12:45:09 -0300

    Adicionar módulo de análise descritiva

---
+++ data-exploration/analises/descritiva/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+''' 
+	Módulo com funções para calculo de métricas descritivas
+'''
+
+__all__ = ['medida_central', 'medida_dispersao']
+__version__ = '0.1'
+__author__ = 'Disciplina Data Science'
