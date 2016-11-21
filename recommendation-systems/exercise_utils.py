883c6cc5a1dedfe2ff3873d63eba26f0291ca651
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2016-11-21 00:50:22 -0300

    material de sistemas de recomendação

---
+++ recommendation-systems/exercise_utils.py
@@ -0,0 +1,28 @@
+from math import sqrt
+import pandas as pd
+
+def get_users_interests():
+    return pd.read_csv('interest.csv', sep=',', header=None, index_col=0).values
+
+def get_users_interests_poor():
+    return [
+        ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
+        ["NoSQL", "MongoDB", "Cassandra", "HBase", "Postgres"],
+        ["Python", "scikit-learn", "scipy", "numpy", "statsmodels", "pandas"],
+        ["R", "Python", "statistics", "regression", "probability"],
+        ["machine learning", "regression", "decision trees", "libsvm"],
+        ["Python", "R", "Java", "C++", "Haskell", "programming languages"],
+        ["statistics", "probability", "mathematics", "theory"],
+        ["machine learning", "scikit-learn", "Mahout", "neural networks"],
+        ["neural networks", "deep learning", "Big Data", "artificial intelligence"],
+        ["Hadoop", "Java", "MapReduce", "Big Data"],
+        ["statistics", "R", "statsmodels"],
+        ["C++", "deep learning", "artificial intelligence", "probability"],
+        ["pandas", "R", "Python"],
+        ["databases", "HBase", "Postgres", "MySQL", "MongoDB"],
+        ["libsvm", "regression", "support vector machines"]
+    ]
+
+def print_matrix(m):
+    for r in m:
+        print r
